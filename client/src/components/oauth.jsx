e2bf47d1a166a53838d9e2c8467d9089c5d5d3e5
Sebastien <perigon@hotmail.com>
2021-03-13 11:42:01 -0500

    login button is change user state to true so the logout button is rendered

--- client/src/components/oauth.jsx
+++ client/src/components/oauth.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GoogleLogin from "react-google-login";
+import { GoogleLogout } from "react-google-login";
 
-const responseGoogle = (response) => {
-  console.log(response.profileObj);
-};
+let logmein = true;
+
 function Oauth() {
-  return (
-    <div className="App">
-      <GoogleLogin
-        clientId="678724353481-e9tdq2si3r8rfabm0bbn768enjt6qrt4.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-      />
-    </div>
-  );
+  const [user, setUser] = useState(false);
+
+  const responseGoogle = (response) => {
+    console.log(response.profileObj);
+    if (response.profileObj.name) {
+      // DOTO imprement and axios call to the backend to validate if the user exist or not. If user does not exist we need to be able
+      //  to send user to the registration page
+      // ?? do we need to send user data into a cookie ??
+      console.log(response.profileObj.googleId);
+      console.log(response.profileObj.name);
+      console.log(response.profileObj.email);
+      setUser(response.profileObj.name);
+    }
+  };
+  if (!user) {
+    return (
+      <div className="App">
+        <GoogleLogin
+          clientId="678724353481-e9tdq2si3r8rfabm0bbn768enjt6qrt4.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <GoogleLogout
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          buttonText="Logout"
+        ></GoogleLogout>
+      </div>
+    );
+  }
 }
 
 export default Oauth;
