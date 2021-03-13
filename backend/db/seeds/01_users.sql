dfccc387b435cdaae9e63f1a47ef07b07e9b576f
paarmin <parniaa@gmail.com>
2021-03-13 12:18:26 -0500

    db seeds done

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,18 +1,11 @@
 INSERT INTO
 users(full_name, photo, email, password, created_at, modified_at, service_range, isServiceProvider)
 VALUES
-    ('Mario','phototest', 'mario@nintendo.com', 'test', 1, 2, 4, false),
-    ('Luigi','phototest', 'luigi@nintendo.com', 'test', 1, 2, 4, false),
-    (
-        'Princess',
-        'phototest',
-        'peach@nintendo.com',
-        'test', 1, 2, 4, false
-    ),
-    ('Princess', 
-        'phototest',
-        'daisy@nintendo.com',
-        'test', 1, 2, 4, false
-    ),
-    ('Donkey', 'phototest', 'donkey@nintendo.com', 'test', 1, 2, 4, false);
+    ('Mario','phototest1', 'mario@nintendo.com', 'test', 1612137600, 1612137600, 15, false),
+    ('Luigi','phototest2', 'luigi@nintendo.com', 'test', 1620072200, 1620072200, 14, true),
+    ('Princess','phototest3','peach@nintendo.com','test', 1522072200, 1460072200, 40, false),
+    ('Princess', 'phototest4', 'daisy@nintendo.com','test', 1620072200, 1530072200, 24, true),
+    ('Monkey', 'phototest5', 'monkey@nintendo.com', 'test', 1520072200, 1620072200, 44, false),
+    ('Dog', 'phototest5', 'dog@nintendo.com', 'test', 1520072200, 1620072200, 64, false),
+    ('Bull', 'phototest5', 'bull@nintendo.com', 'test', 1520072200, 1620072200, 104, false);
 
