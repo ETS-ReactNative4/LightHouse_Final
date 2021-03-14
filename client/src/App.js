8c839e37c3f1556b2cb71c8f6481158e0f546489
Sebastien <perigon@hotmail.com>
2021-03-14 19:02:55 -0400

    app.js bugfix, login button was bronken

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import {useState} from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import useApplicationData from "./hooks/useApplicationData";
 import Oauth from "./components/Oauth";
 import { hasOnlyExpressionInitializer } from "typescript";
-
 
 import Header from "./components/common/Header";
 import Login from "./components/Login";
@@ -21,8 +14,7 @@ import Availability from "./components/Availability";
 import NewServiceForm from "./components/NewServiceForm";
 
 import "./App.css";
-import PageNotFound from './components/PageNotFound';
-
+import PageNotFound from "./components/PageNotFound";
 
 const App = () => {
   const { state, dispatch } = useApplicationData();
@@ -33,54 +25,36 @@ const App = () => {
     </li>
   ));
   const [user, setUser] = useState(null);
-  
-  
-=======
-import "./App.css";
-import React, { useState } from "react";
-
-import Oauth from "./components/oauth";
-const App = () => {
-  const [user, setUser] = useState(false);
-
   const logout = () => {
     setUser(false);
   };
 
->>>>>>> main
   return (
-    
     <Router>
-    <div className="App">
-<<<<<<< HEAD
-      <Header/>
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/register" >
-          <Register setUser={setUser}/> 
-          <Oauth />
-        </Route>
-        <Route path="/login" >
-          <Login setUser={setUser}/>
-          <Oauth />
-        </Route>       
-        <Route path="/services" >
-          <Services/>
-        </Route>       
-        <Route path="/availability" >
-        <Availability/>
-          
-        </Route>       
-        <Route path="/services/:service_id" >
-          <NewServiceForm/>
-          
-        </Route>
-        <Route component={PageNotFound}/>
-      </Switch>
-=======
-      <Oauth user={user} setUser={setUser} logout={logout} />
->>>>>>> main
-    </div>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/register">
+            <Register setUser={setUser} />
+            <Oauth user={user} setUser={setUser} logout={logout} />
+          </Route>
+          <Route path="/login">
+            <Login setUser={setUser} />
+            <Oauth user={user} setUser={setUser} logout={logout} />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/availability">
+            <Availability />
+          </Route>
+          <Route path="/services/:service_id">
+            <NewServiceForm />
+          </Route>
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
     </Router>
   );
 };
