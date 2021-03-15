e854317e12feecd772f0dfbd9c3fd972c915777e
paarmin <parniaa@gmail.com>
2021-03-15 16:18:47 -0400

    more routes

--- client/src/components/Availability/index.js
+++ client/src/components/Availability/index.js
@@ -1,3 +1,16 @@
+import useApplicationData from "../../hooks/useApplicationData"
+
 export default function Availability(props) {
-  return (<h1>I am Availability</h1>);
+
+  const { state, dispatch } = useApplicationData();
+  const userList = state.users.map((user) => (<li key={user.id} > {user.city} {user.last_name} {user.email} </li>
+));
+
+  return (
+  <div className="App" >  
+    <h1>I am login</h1>
+    <ul> {userList} </ul>
+  </div>
+  );
+
 }
