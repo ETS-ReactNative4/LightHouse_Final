c651c15dc34ce592f8e85b99398b438b53393f84
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-03-15 18:08:36 -0400

    add footer

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,10 +2,8 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Oauth from "./Oauth";
-import {Button} from "react-bootstrap";
 
 export default function Header(props) {
-  //check to is if user is logged in or not
   return (
     <>
       <Navbar bg="light" expand="lg" sticky="top">
