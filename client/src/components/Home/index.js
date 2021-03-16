93e1a6b277282c353a2043ab4103b0c5914aa07e
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-03-16 10:33:21 -0400

    add banner to landing page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,5 +1,18 @@
 import "./Home.scss";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
 
 export default function Home(props) {
-  return <h1 className="head">I am Home TOO</h1>;
+  return (
+    <Jumbotron>
+      <h1>Hello, world!</h1>
+      <p>
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </p>
+      <p>
+        <Button variant="primary">Learn more</Button>
+      </p>
+    </Jumbotron>
+  );
 }
