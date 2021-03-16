b55b4f9164e02c4e27de0c6709f80b7f65ce7eda
Sebastien <perigon@hotmail.com>
2021-03-16 12:35:21 -0400

    repaired login button and prefilling

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useHistory } from "react-router-dom";
 
 export default function Register(props) {
-  console.log("this is props of register",props);
+  console.log("this is props of register", props);
   const history = useHistory();
   const getLocation = (e) => {
     e.preventDefault();
@@ -36,76 +36,73 @@ export default function Register(props) {
   const registration = (event) => {
     event.preventDefault();
     console.log("tt", event);
-  }
+  };
   return (
     <>
       <h1>I am Register</h1>
-      {props.user  && props.user.register ? (
-      
-      <form method="POST" action="/api/register">
-        <div>
-          <label>
-            Full Name:
-            <input type="text" name="name"  />
-            {/* value={props.user.name} */}
-          </label>
-          <label>
-            Email:
-            <input type="text" name="name" />
-            {/* value={props.user.email}  */}
-          </label>
-        </div>
-        <div>
-          <input type="checkbox" id="service" name="service"></input>
-          <label for="service">Do you wish to provide services ?</label>
-        </div>
-        <div>
-          <button onClick={getLocation}>Get my current location</button>
-        </div>
-        <div>
-          <label>
-            Address
-            <input
-              type="text"
-              name="full_address"
-              value={props.location && props.location.address}
-            />
-          </label>
-          <label>
-            City
-            <input
-              type="text"
-              name="city"
-              value={props.location && props.location.city}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Postal code
-            <input
-              type="text"
-              name="postal_code"
-              value={props.location && props.location.postal}
-            />
-          </label>
-          <label>
-            Country
-            <input
-              type="text"
-              name="country"
-              value={props.location && props.location.country}
-            />
-          </label>
-        </div>
-        <div>
-          <button type="submit" >Register</button>
-        </div>
-
-      </form>)
-
-    : history.push('/home') }
-     {/* <h2>{props.user.email}</h2>
+      {props.user && props.user.register ? (
+        <form method="POST" action="/api/register">
+          <div>
+            <label>
+              Full Name:
+              <input type="text" name="name" value={props.user.name} />
+            </label>
+            <label>
+              Email:
+              <input type="text" name="name" value={props.user.email} />
+            </label>
+          </div>
+          <div>
+            <input type="checkbox" id="service" name="service"></input>
+            <label for="service">Do you wish to provide services ?</label>
+          </div>
+          <div>
+            <button onClick={getLocation}>Get my current location</button>
+          </div>
+          <div>
+            <label>
+              Address
+              <input
+                type="text"
+                name="full_address"
+                value={props.location && props.location.address}
+              />
+            </label>
+            <label>
+              City
+              <input
+                type="text"
+                name="city"
+                value={props.location && props.location.city}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Postal code
+              <input
+                type="text"
+                name="postal_code"
+                value={props.location && props.location.postal}
+              />
+            </label>
+            <label>
+              Country
+              <input
+                type="text"
+                name="country"
+                value={props.location && props.location.country}
+              />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Register</button>
+          </div>
+        </form>
+      ) : (
+        history.push("/home")
+      )}
+      {/* <h2>{props.user.email}</h2>
   <h2>{props.user.full_name}</h2> */}
     </>
   );
