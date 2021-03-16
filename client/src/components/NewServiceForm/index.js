15659cd995765ce28969732e9c1067c41d634ce9
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-03-16 16:21:43 -0400

    add state for each input, pass states in object with axios call

--- client/src/components/NewServiceForm/index.js
+++ client/src/components/NewServiceForm/index.js
@@ -3,29 +3,26 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
+import axios from "axios";
 
 export default function NewServiceForm(props) {
-  const [state, setState] = useState({
-    title: null,
-    description: null,
-    fee: null,
-    category: null,
-  });
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [fee, setFee] = useState("");
+  const [category, setCategory] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = this.state;
-    // console.log(this.inputFullNameRef.current.value)
+    const data = {
+      formTitle: title,
+      formDescription: description,
+      formFee: fee,
+      formCategory: category,
+    };
     console.log("Final data is", data);
-  };
 
-  handleInputChange = (event) => {
-    event.preventDefault();
-    // console.log(event)
-    // console.log(event.target.name)
-    // console.log(event.target.value)
-    setState({
-      [event.target.name]: event.target.value,
+    axios.post("/api/services", {data}).then((response) => {
+      console.log("success!!");
     });
   };
 
@@ -38,6 +35,7 @@ export default function NewServiceForm(props) {
             type="text"
             name="title"
             placeholder="Enter title of the service you would like to provide"
+            onChange={(event) => setTitle(event.target.value)}
           />
         </Form.Group>
 
@@ -47,13 +45,18 @@ export default function NewServiceForm(props) {
             type="text"
             name="description"
             placeholder="Enter the description of your service"
+            onChange={(event) => setDescription(event.target.value)}
           />
         </Form.Group>
       </Form.Row>
 
       <Form.Group controlId="formGridFee">
         <Form.Label>Fee</Form.Label>
-        <Form.Control name="fee" placeholder="Please enter the amount in $" />
+        <Form.Control
+          name="fee"
+          placeholder="Please enter the amount in $"
+          onChange={(event) => setFee(event.target.value)}
+        />
       </Form.Group>
 
       <Form.Group controlId="formGridCategory">
@@ -61,6 +64,7 @@ export default function NewServiceForm(props) {
         <Form.Control
           name="category"
           placeholder="Please enter the category name for your serice, i.e: plumbing"
+          onChange={(event) => setCategory(event.target.value)}
         />
       </Form.Group>
 
