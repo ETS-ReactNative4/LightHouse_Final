4edad7baa5c291f3bff6266c06bb658ebb2e1b25
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-03-16 12:52:05 -0400

    add cards to landing page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,18 +1,66 @@
 import "./Home.scss";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 export default function Home(props) {
   return (
-    <Jumbotron>
-      <h1>Hello, world!</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn more</Button>
-      </p>
-    </Jumbotron>
+    <>
+      <Jumbotron>
+        <h1>This why you should use us!</h1>
+        <p>We will help you </p>
+        <p>
+          <Button variant="primary" href="/register">
+            Register
+          </Button>
+        </p>
+      </Jumbotron>
+      <Container fluid className="main-cards">
+        <Row>
+          <Col sm className="card-item">
+            <Card style={{width: "18rem"}}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm className="card-item">
+            <Card style={{width: "18rem"}}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm className="card-item">
+            <Card style={{width: "18rem"}}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
