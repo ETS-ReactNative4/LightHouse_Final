54421e71fecbc0c0dd05b2bdbddb5dc35b3cfddb
Sebastien <perigon@hotmail.com>
2021-03-18 11:43:03 -0400

    refactoring and testing of the refactoring of the avalibility schema and seeds

--- backend/db/schema/05_availabilities.sql
+++ backend/db/schema/05_availabilities.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS availabilities CASCADE;
 CREATE TABLE availabilities(
     id SERIAL PRIMARY KEY NOT NULL,
-    start_time TIMESTAMPTZ DEFAULT Now(),
-    end_time TIMESTAMPTZ DEFAULT Now(),
-    available_date TIMESTAMPTZ DEFAULT Now(),
+    start_time int,
+    end_time int,
+
     created_at TIMESTAMPTZ DEFAULT Now() ,
     modified_at TIMESTAMPTZ DEFAULT Now() ,
     services_id INTEGER REFERENCES services(id) ON DELETE CASCADE
