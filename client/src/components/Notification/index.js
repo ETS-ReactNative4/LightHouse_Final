4011f3f13fb28f4e57d5dc4064744c827f8b130c
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-03-19 16:01:49 -0400

    add useeffect, and axios call to get all apps

--- client/src/components/Notification/index.js
+++ client/src/components/Notification/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import {BsFillBellFill} from "react-icons/bs";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 export default function Notification(props) {
+  useEffect(() => {
+    axios.get("/api/appointments/").then((response) => {
+      console.log("success!!");
+      console.log("THIS IS RESPONSE:", response.data.length);
+    });
+  }, []);
+
   return <BsFillBellFill />;
 }
