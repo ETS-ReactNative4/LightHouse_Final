02b53bb852004fcabe7b6363a0a4be7d511b306e
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-03-19 15:40:33 -0400

    add useState hook

--- client/src/components/Notification/index.js
+++ client/src/components/Notification/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {BsFillBellFill} from "react-icons/bs";
+import {useState} from "react";
 
 export default function Notification(props) {
   return <BsFillBellFill />;
