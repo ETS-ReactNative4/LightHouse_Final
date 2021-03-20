c3bdf99b942c717d207b3b345dece5243c706115
paarmin <parniaa@gmail.com>
2021-03-20 07:15:24 -0400

    categories seed create

---
+++ backend/db/seeds/07_categories.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+categories (title)
+VALUES
+    ('Accounting'),
+    ('House Cleaning'),
+    ('House Repair'),
+    ('Tutoring'),
+    ('Free Help'),
+    ('Car Reapair'),
+    ('Programing'),
+    ('Shopping');
+
+
+
